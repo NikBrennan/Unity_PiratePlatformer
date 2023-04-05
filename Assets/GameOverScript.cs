92c6571f0e06f706dc9c7a5a354d44f3b1168582
Preston-Beck <pgbeck@lakeheadu.ca>
2023-04-05 12:24:26 -0400

    Added Coin Count to GameOverScene

--- Assets/GameOverScript.cs
+++ Assets/GameOverScript.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameOverScript : MonoBehaviour
 {
     public GameObject gameOverUI;
+    public Text Score;
+    private float score = CoinPicker.coins;
+
+    private void Start()
+    {
+        Score.text += score.ToString();
+    }
 
     public void Restart()
     {
@@ -22,6 +30,4 @@ public class GameOverScript : MonoBehaviour
         Debug.Log("Quitting Game...");
         Application.Quit();
     }
-
-
 }
